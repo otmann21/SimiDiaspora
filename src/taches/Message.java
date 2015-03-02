29320b3e1ca734ef52002a4a688498b31b351f29
otmann21 <otmann.tnitan@gmail.com>
2015-03-02 14:13:15 +0100

    Ajout de 'type' et 'code' à la tache Message.

--- src/taches/Message.java
+++ src/taches/Message.java
@@ -4,7 +4,8 @@ import org.simgrid.msg.Task;
 import org.simgrid.msg.TaskCancelledException;
 
 /**
- * 
+ * Cette classe rassemble tous les messages que les peers peuvent s'envoyer. Elle est bordelique, il va falloir mieux la structurer, 
+ * avec notamment cet argument type qui donne le type du message (texte, haché, code...)
  * @author otmann
  *
  */
@@ -12,15 +13,25 @@ public class Message extends Task{
 	
 	protected String message;
 	protected byte[] hash;
+	protected byte[] code;
+	protected int type;
 	
 	public Message(String message){
 		super();
 		this.message = message;
+		this.type=0;
 	}
 	
 	public Message(byte[] hash){
 		super();
 		this.hash=hash;
+		this.type=1;
+	}
+	
+	public Message(byte[] code, int type){
+		super();
+		this.code=code;
+		this.type=2;
 	}
 	
 	public void execute() throws HostFailureException,TaskCancelledException {
@@ -35,4 +46,11 @@ public class Message extends Task{
 		return hash;
 	}
 	
+	public int getType(){
+		return type;
+	}
+	
+	public byte[] getCode(){
+		return code;
+	}
 }
