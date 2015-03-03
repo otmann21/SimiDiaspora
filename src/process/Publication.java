a908d3ae8d750b00ecb8cf87fc91041e210ff2a0
otmann21 <otmann.tnitan@gmail.com>
2015-03-03 15:40:52 +0100

    Process.publication avance

--- src/process/Publication.java
+++ src/process/Publication.java
@@ -1,13 +1,67 @@
 package process;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 
-public class Publication {
+import org.simgrid.msg.Host;
+import org.simgrid.msg.HostFailureException;
+import org.simgrid.msg.Msg;
+import org.simgrid.msg.MsgException;
+import org.simgrid.msg.Process;
+import org.simgrid.msg.Task;
+import org.simgrid.msg.TimeoutException;
+import org.simgrid.msg.TransferFailureException;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import taches.AjouteDonnee;
+import taches.Message;
+/**
+ * On envoie une publi sur le sp.
+ * args[0] : le sp ; args[1] : la donnee
+ * 
+ * @author otmann
+ *
+ */
+public class Publication extends Process {
 
+	private byte[] monCode;
+	private String mboxPublication;
+	private HashMap<String, byte[]> amis;
+
+
+	public Publication(Host host, String name, String[]args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		super(host,name,args);
+		this.amis=(HashMap<String, byte[]>) host.getData();
+		monCode = new byte[]{42, 23, 76, 21};
+		MessageDigest md = MessageDigest.getInstance("SHA-1");
+		md.reset();
+		md.update(monCode);
+		mboxPublication = new String(md.digest(), "UTF-8");
 	}
 
+	public void main(String[] args) throws MsgException {
+
+		AjouteDonnee env = new AjouteDonnee(args[1]);
+		Msg.info(Host.currentHost().getName()+" ajoute une donnée sur " + args[0] + ".");
+		env.send(args[0]);
+		MessageDigest md;
+		try {//on met le hash de la publi sur ma mbox de publi
+			md = MessageDigest.getInstance("SHA-1");
+			md.reset();
+			md.update(args[1].getBytes());
+			Msg.info(Host.currentHost().getName()+" donne à ses amis le hash de l'info qu'il a ajouté sur " + args[0] + ".");
+			(new Message(md.digest(), 1)).send(this.mboxPublication);
+
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		//je ping mes potos pour leur dire que j'ai publié un truc
+		for(String ami : this.amis.keySet()){
+			Message ping = new Message("ping");
+			ping.setNomPeer(host.getName());
+			ping.send(ami);
+		}
+	}
 }
--- src/process/Reception.java
+++ src/process/Reception.java
@@ -26,27 +26,43 @@ import taches.Requete;
 
 public class Reception extends Process {
 
-	private byte[] code;
+	private byte[] monCode;
 	private String mbox;
 	private HashMap<String, byte[]> amis;
 
 	public Reception(Host host, String name, String[]args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		super(host,name,args);
 
-		code = new byte[]{42, 23, 76, 21};
+		monCode = new byte[]{42, 23, 76, 21};
 		MessageDigest md = MessageDigest.getInstance("SHA-1");
 		md.reset();
-		md.update(code);
+		md.update(monCode);
 		mbox = new String(md.digest(), "UTF-8");
 		amis = new HashMap<String, byte[]>();
 	}
 
 	public void main(String[] args) throws MsgException {
 		Msg.info(Host.currentHost().getName() + " regarde si son ami peer0 publie qqch.");
-		Task.listen(mbox);
-		Task msg = Task.receive(mbox);
+		Task.listen(host.getName());
+		Task msg = Task.receive(host.getName());
 		if(msg instanceof Message){
-			if(((Message) msg).getType()==1){ //Un ami nous dit qu'il
+			if(((Message) msg).getType()==0 && ((Message) msg).getMessage().equals("ping")){//on a été pingué par un poto
+				//On vérifie si on le code du peer qui nous a pingé
+				if(amis.containsKey(((Message) msg).getNomPeer()) && amis.get(((Message) msg).getNomPeer())!=null)
+				{
+					try {
+						String mboxDuPeer = codeToMbox(amis.get(((Message) msg).getNomPeer()));
+					} catch (NoSuchAlgorithmException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (UnsupportedEncodingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			if(((Message) msg).getType()==1){ //Un ami nous dit qu'il a publié un truc
 				byte[] hash = ((Message) msg).getHash();
 				Requete req = new Requete(hash);
 				Msg.info(Host.currentHost().getName() + " voit que peer0 a mis qqch sur " + args[0] + ". Il fait une requete a " + args[0] + " en se servant du hash que peer0 a distribué à ses amis.");
@@ -58,7 +74,7 @@ public class Reception extends Process {
 					Msg.info(Host.currentHost().getName() + " a récupéré sur " + args[0] + " la donnée : \"" + contenu + "\".");
 				}
 			}
-			else if(((Message) msg).getType()==2){
+			else if(((Message) msg).getType()==2){//Un ami s'est co et le sp nous envoie son code
 				if(this.amis.containsKey(((Message) msg).getNomPeer())){
 					this.amis.put(((Message)msg).getNomPeer(),((Message)msg).getCode());
 				}
@@ -71,8 +87,8 @@ public class Reception extends Process {
 	 * Le peer se connecte à son superpeer pour lui donner son code et obtenir ses amis.
 	 */
 	public void connexion(String sp) throws TransferFailureException, HostFailureException, TimeoutException{
-		this.code = new byte[]{42, 23, 76, 21};
-		Message envoiCode = new Message(this.code, 2);
+		this.monCode = new byte[]{42, 23, 76, 21};
+		Message envoiCode = new Message(this.monCode, 2);
 		envoiCode.send(sp);
 		Task.listen(Host.currentHost().getName());
 		Task listeAmis = Task.receive(Host.currentHost().getName());
@@ -81,5 +97,14 @@ public class Reception extends Process {
 				this.amis.put(a, null);
 			}
 		}
+		host.setData(this.amis);
+	}
+	
+	public String codeToMbox(byte[] code) throws NoSuchAlgorithmException, UnsupportedEncodingException{
+		MessageDigest md = MessageDigest.getInstance("SHA-1");
+		md.reset();
+		md.update(code);
+		String res = new String(md.digest(), "UTF-8");
+		return res;
 	}
 }
